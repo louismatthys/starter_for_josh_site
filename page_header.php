100b34a1e0cbca56ee1b1f38c74054f88a60db75
louismatthys <louis.10matthys@gmail.com>
2025-03-20 18:43:34 +0800

    Update page_header.php
    fix tags

--- page_header.php
+++ page_header.php
@@ -90,7 +90,7 @@
         </div>
       </nav>
 <div class="smallpage-tag">
-      <h1 class=smallpage-title>Projects</h1>
+      <h1 class="smallpage-title">Projects</h1>
     </div>
 </div> <!-- container -->
 <?php wp_head(); ?> <!-- very important that this code is added-->
